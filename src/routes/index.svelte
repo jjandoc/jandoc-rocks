c37f26ecb77b2c797e802871c9f81aaf53614834
Jon Jandoc <jon.jandoc@gmail.com>
2020-10-13 23:24:20 -0700

    Add NetlifyCMS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', (user) => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+  });
+</script>
+
 <style>
   :global(.main-content.home) {
     padding: 0;
@@ -32,6 +48,8 @@
 
 <svelte:head>
   <title>Jon Jandoc Rocks!</title>
+  <script src="https://identity.netlify.com/v1/netlify-identity-widget.js">
+  </script>
 </svelte:head>
 
 <h1>
