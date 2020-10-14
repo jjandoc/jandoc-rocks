20f9ae9ea650ad6b9afcfb22b7343b9df54d94ff
Jon Jandoc <jon.jandoc@gmail.com>
2020-10-13 23:43:11 -0700

    Update NetlifyCMS config

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -1,25 +1,33 @@
-import fs from 'fs';
+import fm from 'front-matter';
+import glob from 'glob';
+import { fs } from 'mz';
 import path from 'path';
-import grayMatter from 'gray-matter';
 
-const getAllPosts = () => {
-  try {
-    return fs.readdirSync('static/posts/').map((fileName) => {
-      const post = fs.readFileSync(
-        path.resolve('static/posts', fileName),
-        'utf-8',
-      );
-      return grayMatter(post).data;
-    });
-  } catch (e) {
-    return [];
-  }
-};
+export async function get(req, res) {
+  // List the Markdown files and return their filenames
+  const posts = await new Promise((resolve, reject) =>
+    glob('static/_posts/*.md', (err, files) => {
+      if (err) return reject(err);
+      return resolve(files);
+    }),
+  );
 
-export function get(_, res) {
+  // Read the files and parse the metadata + content
+  const postsFrontMatter = await Promise.all(
+    posts.map(async (post) => {
+      const content = (await fs.readFile(post)).toString();
+      // Add the slug (based on the filename) to the metadata, so we can create links to this blog post
+      return { ...fm(content).attributes, slug: path.parse(post).name };
+    }),
+  );
+
+  // Sort by reverse date, because it's a blog
+  postsFrontMatter.sort((a, b) => (a.date < b.date ? 1 : -1));
+
   res.writeHead(200, {
     'Content-Type': 'application/json',
   });
-  const posts = getAllPosts();
-  res.end(JSON.stringify(posts));
+
+  // Send the list of blog posts to our Svelte component
+  res.end(JSON.stringify(postsFrontMatter));
 }
